85e0e308dd7473f5a2300819a429ba4962f80be4
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2025-07-12 19:04:20 +0300

    Add solution for N-th Element task

---
+++ 03.Arrays/01.printNthElement.js
@@ -0,0 +1,14 @@
+function printNthElement(arr) {
+    let step = Number(arr.pop());
+
+    let result = [];
+    for (let i = 0; i < arr.length; i += step) {
+        result.push(arr[i]);
+    }
+
+    console.log(result.join(' '));
+}
+
+printNthElement(['5', '20', '31', '4', '20', '2']);
+printNthElement(['dsa', 'asd', 'test', 'test', '2']);
+printNthElement(['1', '2', '3', '4', '5', '6']);         

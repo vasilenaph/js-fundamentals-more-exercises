8de09a8cc7f6c2311c878ecbc496a8198d7c1b03
Vasilena Todorova <vasilenaph@users.noreply.github.com>
2025-07-12 15:21:27 +0300

    Solve Biggest of 3 Numbers

---
+++ Data-Types-And-Variables/03.biggestOfThreeNums.js
@@ -0,0 +1,8 @@
+function biggesOfThreeNums(num1, num2, num3) {
+    console.log(Math.max(num1, num2, num3));
+}
+
+biggesOfThreeNums(-2, 7, 3);
+biggesOfThreeNums(130, 5, 99);
+biggesOfThreeNums(43, 43.2, 43.1);
+biggesOfThreeNums(2, 2, 2);
